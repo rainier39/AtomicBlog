336df68b2e4aa98806aa0b7b4ec0daad34162581
rainier39 <196280087+rainier39@users.noreply.github.com>
2025-07-23 15:13:57 -0700

    Fix formatting

--- core/functions.php
+++ core/functions.php
@@ -1,1 +1,15 @@
-<?php// functions.php// Defines global functions used throughout the software.// Only load the page if it's being requested via the index file.if (!defined('INDEX')) exit;// Log a user out.function logout() {    session_unset();    session_destroy();}?>
+<?php
+// functions.php
+// Defines global functions used throughout the software.
+
+// Only load the page if it's being requested via the index file.
+if (!defined('INDEX')) exit;
+
+// Log a user out.
+function logout() {
+    session_unset();
+    session_destroy();
+}
+
+?>
+
