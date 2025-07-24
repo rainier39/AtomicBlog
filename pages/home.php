542954af524dd1281f2bd21ab0e6bdb131640f1d
rainier39 <196280087+rainier39@users.noreply.github.com>
2025-07-24 15:01:35 -0700

    Handle no posts existing, fix errors

--- pages/home.php
+++ pages/home.php
@@ -11,9 +11,16 @@ $starred = $db->query("SELECT * FROM `posts` WHERE starred='1'");
 // Display the starred posts fieldset.
 echo("<fieldset class='starred'><legend>Starred Posts</legend>");
 
-// Display the starred posts themselves.
-while ($s = $starred->fetch_assoc()) {
-    echo("<div class='posts'><a href='/post/" . $s["id"] . "/'><img src='/images/" . $s["id"] . "." . $s["icon"] . "'></a></br><a href='/post/" . $s["id"] . "/'>" . $s["title"] . "</a></div>");
+// Only display the posts if there are any.
+if ($starred->num_rows > 0) {
+    // Display the starred posts themselves.
+    while ($s = $starred->fetch_assoc()) {
+        echo("<div class='posts'><a href='/post/" . $s["id"] . "/'><img src='/images/" . $s["id"] . "." . $s["icon"] . "'></a></br><a href='/post/" . $s["id"] . "/'>" . $s["title"] . "</a></div>");
+    }
+}
+// Otherwise print a message.
+else {
+    echo("No starred posts yet.");
 }
 
 // End the starred posts fieldset.
@@ -25,46 +32,56 @@ $views = array();
 // Get all the postids.
 $postids = $db->query("SELECT * FROM `posts`");
 
-// Populate the array.
-while ($p = $postids->fetch_assoc()) {
-    $views[$p["id"]] = 0;
-}
+// If there are any posts...
+if ($postids->num_rows > 0) {
+    // Populate the array.
+    while ($p = $postids->fetch_assoc()) {
+        $views[$p["id"]] = 0;
+    }
 
-// Get all of the views, and just get their posts.
-$posts = $db->query("SELECT post FROM `views`");
+    // Get all of the views, and just get their posts.
+    $posts = $db->query("SELECT post FROM `views`");
 
-// Fill the array with the proper amount of views per post.
-while ($p = $posts->fetch_assoc()) {
-    $views[$p["post"]] += 1;
-}
+    // Fill the array with the proper amount of views per post.
+    while ($p = $posts->fetch_assoc()) {
+        $views[$p["post"]] += 1;
+    }
 
-// Make a second array consisting of the 5 highest viewed posts, namely their ids.
-$mostViewed = array();
+    // Make a second array consisting of the 5 highest viewed posts, namely their ids.
+    $mostViewed = array();
 
-// Populate the array.
-for ($i = 0; $i < 5; $i++) {
-    // Find the highest viewed post.
-    $value = max($views);
+    // Populate the array.
+    for ($i = 0; $i < 5; $i++) {
+        // Find the highest viewed post.
+        $value = max($views);
 
-    // Get its key.
-    $key = array_search($value, $views);
+        // Get its key.
+        $key = array_search($value, $views);
 
-    // Remove it from the array.
-    unset($views[$key]);
+        // Remove it from the array.
+        unset($views[$key]);
 
-    // Add the postid to the new array.
-    $mostViewed[$i] = $key;
+        // Add the postid to the new array.
+        $mostViewed[$i] = $key;
+    }
 }
 
 // Display the most viewed fieldset.
 echo("</br><fieldset class='mostViewed'><legend>Most Viewed</legend>");
 
-// Get the posts we wish to display.
-$mostViewedPosts = $db->query("SELECT * FROM `posts` WHERE id='" . $mostViewed[0] . "' OR id='" . $mostViewed[1] . "' OR id='" . $mostViewed[2] . "' OR id='" . $mostViewed[3] . "' OR id='" . $mostViewed[4] . "' ORDER BY FIELD(ID," . $mostViewed[0] . ", " . $mostViewed[1] . ", " . $mostViewed[2] . ", " . $mostViewed[3] . ", " . $mostViewed[4] . ")");
+// Only try to display posts if there are any.
+if ($postids->num_rows > 0) {
+    // Get the posts we wish to display.
+    $mostViewedPosts = $db->query("SELECT * FROM `posts` WHERE id='" . $mostViewed[0] . "' OR id='" . $mostViewed[1] . "' OR id='" . $mostViewed[2] . "' OR id='" . $mostViewed[3] . "' OR id='" . $mostViewed[4] . "' ORDER BY FIELD(ID," . $mostViewed[0] . ", " . $mostViewed[1] . ", " . $mostViewed[2] . ", " . $mostViewed[3] . ", " . $mostViewed[4] . ")");
 
-// Display the posts.
-while ($m = $mostViewedPosts->fetch_assoc()) {
-    echo("<div class='posts'><a href='/post/" . $m["id"] . "/'><img src='/images/" . $m["id"] . "." . $m["icon"] . "'></a></br><a href='/post/" . $m["id"] . "/'>" . $m["title"] . "</a></div>");
+    // Display the posts.
+    while ($m = $mostViewedPosts->fetch_assoc()) {
+        echo("<div class='posts'><a href='/post/" . $m["id"] . "/'><img src='/images/" . $m["id"] . "." . $m["icon"] . "'></a></br><a href='/post/" . $m["id"] . "/'>" . $m["title"] . "</a></div>");
+    }
+}
+// Otherwise print a message.
+else {
+    echo("No posts yet.");
 }
 
 // End the fieldset.
@@ -76,13 +93,19 @@ $recent = $db->query("SELECT * FROM `posts` ORDER BY starttime DESC LIMIT 5");
 // Display the most recent fieldset.
 echo("</br><fieldset class='mostRecent'><legend>Most Recent</legend>");
 
-// Display the posts.
-while ($r = $recent->fetch_assoc()) {
-    echo("<div class='posts'><a href='/post/" . $r["id"] . "/'><img src='/images/" . $r["id"] . "." . $r["icon"] . "'></a></br><a href='/post/" . $r["id"] . "/'>" . $r["title"] . "</a></div>");
+// Only try to display posts if there are any.
+if ($recent->num_rows > 0) {
+    // Display the posts.
+    while ($r = $recent->fetch_assoc()) {
+        echo("<div class='posts'><a href='/post/" . $r["id"] . "/'><img src='/images/" . $r["id"] . "." . $r["icon"] . "'></a></br><a href='/post/" . $r["id"] . "/'>" . $r["title"] . "</a></div>");
+    }
+}
+// Otherwise print a message.
+else {
+    echo("No posts yet.");
 }
 
 // End the fieldset.
 echo ("</fieldset>");
 
 ?>
-
