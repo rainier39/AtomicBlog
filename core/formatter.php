ed179d5bfe0b21660d9b6c033a775f955835f41b
rainier39 <196280087+rainier39@users.noreply.github.com>
2025-08-22 01:05:45 -0700

    Complete formatter's markdown implementation

--- core/formatter.php
+++ core/formatter.php
@@ -11,6 +11,8 @@ function format($string) {
     $string = htmlspecialchars($string);
 
     // Apply the various formatter functions to the string.
+    $string = format_list($string);
+    $string = format_numeric_list($string);
     $string = format_bold($string);
     $string = format_italic($string);
     $string = format_code_block($string);
@@ -19,6 +21,8 @@ function format($string) {
     $string = format_subheading($string);
     $string = format_horizontal_rule($string);
     $string = format_blockquote($string);
+    $string = format_image($string);
+    $string = format_link($string);
     $string = format_paragraphs($string);
     $string = format_linebreaks($string);
 
@@ -27,6 +31,14 @@ function format($string) {
 }
 
 // Define the various formatter functions.
+function format_list($string) {
+    return preg_replace("/^\*\s+(.+?)$|^-\s+(.+?)$/mis", "<li>$1$2</li>", $string);
+}
+
+function format_numeric_list($string) {
+    return preg_replace("/^\d+\.\s+(.+?)$|^\d+\)\s+(.+?)$/mis", "<ol start='$0'><li>$1$2</li></ol>", $string);
+}
+
 function format_bold($string) {
     return preg_replace("/\*\*(.+?)\*\*|__(.+?)__/is", "<b>$1$2</b>", $string);
 }
@@ -52,11 +64,19 @@ function format_subheading($string) {
 }
 
 function format_horizontal_rule($string) {
-    return preg_replace("/\-\-\-/is", "<hr>", $string);
+    return preg_replace("/---/is", "<hr>", $string);
 }
 
 function format_blockquote($string) {
     return preg_replace("/^&gt;\s+(.+?)$/mis", "<blockquote>$1</blockquote>", $string);
+}
+
+function format_image($string) {
+    return preg_replace("/!\[(.+?)\]\((http|https):\/\/(.+?)\)/is", "<img src='$2://$3' alt='$1'>", $string);
+}
+
+function format_link($string) {
+    return preg_replace("/\[(.+?)\]\((http|https):\/\/(.+?)\)/is", "<a href='$2://$3' target='_blank'>$1</a>", $string);
 }
 
 function format_paragraphs($string) {
@@ -66,7 +86,5 @@ function format_paragraphs($string) {
 function format_linebreaks($string) {
     return preg_replace("/\s\s$/mis", "</br>", $string);
 }
-
-// TODO: lists, images, links
 
 ?>
