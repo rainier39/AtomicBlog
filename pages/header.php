0b86d8cfb05a792e9acca109f05a7354f380dcbd
rainier39 <196280087+rainier39@users.noreply.github.com>
2025-08-29 18:52:47 -0700

    Remove about and contact pages

--- pages/header.php
+++ pages/header.php
@@ -17,7 +17,7 @@ echo("<!DOCTYPE html>
 echo("<div class='header'><b>" . htmlspecialchars($config["title"]) . "</b></br><small>" . htmlspecialchars($config["description"]) . "</small></div>");
 
 if ($config["installed"]) {
-    echo("<div class='navbar'><a href='/'>Home</a> <a href='/posts/'>Posts</a> <a href='/about/'>About</a> <a href='/contact/'>Contact</a>" . ((isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) ? " <a href='/panel/'>Panel</a> <a href='/logout/'>Logout</a>" : " <a href='/login/'>Login</a> <a href='/register/'>Register</a>") . "</div>");
+    echo("<div class='navbar'><a href='/'>Home</a> <a href='/posts/'>Posts</a>" . ((isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) ? " <a href='/panel/'>Panel</a> <a href='/logout/'>Logout</a>" : " <a href='/login/'>Login</a> <a href='/register/'>Register</a>") . "</div>");
 }
 
 echo("<div class='content'>");
