2d7894a1b9da8c9dcd2577b96451aa6d55206432
rainier39 <196280087+rainier39@users.noreply.github.com>
2025-08-29 17:21:55 -0700

    Fix an oopsie

--- pages/header.php
+++ pages/header.php
@@ -12,12 +12,12 @@ echo("<!DOCTYPE html>
 <meta name='viewport' content='width=device-width,initial-scale=1'>
 <link rel='stylesheet' href='/themes/" . htmlspecialchars($config["theme"]) . "/theme.css'>
 <link rel='icon' type='image/x-icon' href='/themes/" . htmlspecialchars($config["theme"]) . "/icon.png'>
-<body>
+<body>");
 
-<div class='header'><b>" . htmlspecialchars($config["title"]) . "</b></br><small>" . htmlspecialchars($config["description"]) . "</small></div>";
+echo("<div class='header'><b>" . htmlspecialchars($config["title"]) . "</b></br><small>" . htmlspecialchars($config["description"]) . "</small></div>");
 
 if ($config["installed"]) {
-    echo("<div class='navbar'><a href='/'>Home</a> <a href='/posts/'>Posts</a> <a href='/about/'>About</a> <a href='/contact/'>Contact</a>" . ((isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) ? " <a href='/panel/'>Panel</a> <a href='/logout/'>Logout</a>" : " <a href='/login/'>Login</a> <a href='/register/'>Register</a>") . "</div>";
+    echo("<div class='navbar'><a href='/'>Home</a> <a href='/posts/'>Posts</a> <a href='/about/'>About</a> <a href='/contact/'>Contact</a>" . ((isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) ? " <a href='/panel/'>Panel</a> <a href='/logout/'>Logout</a>" : " <a href='/login/'>Login</a> <a href='/register/'>Register</a>") . "</div>");
 }
 
 echo("<div class='content'>");
