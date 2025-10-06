936573bc0ffbb89505f83549596c14a25b7b4656
rainier39 <196280087+rainier39@users.noreply.github.com>
2025-10-06 13:06:07 -0700

    Add CSRF protection, error CSS, better logic

--- pages/login.php
+++ pages/login.php
@@ -6,54 +6,62 @@
 if (!defined('INDEX')) exit;
 
 $content = "";
+$success = false;
 
 // If the user is already logged in, don't let them into the page.
 if (isset($_SESSION["logged_in"]) && ($_SESSION["logged_in"] === true)) {
-   $content .= "You're already logged in.";
+   $content .= "<div class='error'>You're already logged in.</div>";
 }
 // Otherwise, proceed as normal.
 else {
     // Handle requests.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // First find the account they want to log into.
-        $result = $db->query("SELECT * FROM `accounts` WHERE username='" . $db->real_escape_string($_POST["username"]) . "'");
+        // If the CSRF token is sent and valid.
+        if ((isset($_POST["csrf_token"])) and ($_POST["csrf_token"] === $_SESSION["csrf_token"])) {
+            // Generate a new token.
+            generateCSRFToken();
+            // First find the account they want to log into.
+            $result = $db->query("SELECT * FROM `accounts` WHERE username='" . $db->real_escape_string($_POST["username"]) . "'");
 
-        // If the login failed, let them know.
-        if ($result->num_rows < 1) {
-            $content .= "Failed to find requested account.";
-        }
-        // If the login didn't immediately fail, keep going.
-        else {
-            while ($r = $result->fetch_assoc()) {
-                // If the entered password doesn't match with the hash in the database, inform the user.
-                if (!password_verify($_POST["password"], $r["password"])) {
-                    $content .= "The password you entered was incorrect.";
-                }
-                // Otherwise, proceed.
-                else {
-                    // Check if the user is unapproved. If so, don't let them log in.
-                    if ($r["role"] == "Unapproved") {
-                        $content .= "Your account must be approved before you can log in.";
+            // If the login failed, let them know.
+            if ($result->num_rows < 1) {
+                $content .= "<div class='error'>Failed to find requested account.</div>";
+            }
+            // If the login didn't immediately fail, keep going.
+            else {
+                while ($r = $result->fetch_assoc()) {
+                    // If the entered password doesn't match with the hash in the database, inform the user.
+                    if (!password_verify($_POST["password"], $r["password"])) {
+                        $content .= "<div class='error'>The password you entered was incorrect.</div>";
                     }
-                    // Otherwise, log the user in.
+                    // Otherwise, proceed.
                     else {
-                        $_SESSION["logged_in"] = true;
-                        $_SESSION["id"] = $r["id"];
-                        $_SESSION["username"] = $r["username"];
-                        $_SESSION["role"] = $r["role"];
+                        // Check if the user is unapproved. If so, don't let them log in.
+                        if ($r["role"] == "Unapproved") {
+                            $content .= "<div class='error'>Your account must be approved before you can log in.</div>";
+                        }
+                        // Otherwise, log the user in.
+                        else {
+                            $_SESSION["logged_in"] = true;
+                            $_SESSION["id"] = $r["id"];
+                            $_SESSION["username"] = $r["username"];
+                            $_SESSION["role"] = $r["role"];
 
-                        $content .= "Successfully logged in. Welcome, " . htmlspecialchars($_SESSION["username"]) . ".";
+                            $content .= "Successfully logged in. Welcome, " . htmlspecialchars($_SESSION["username"]) . ".";
+                            $success = true;
+                        }
                     }
                 }
             }
         }
     }
     // Display the login form.
-    else {
+    if (!$success) {
         $content .= "
         <div class='loginForm'>
             <h2>Log in</h2>
             <form method='post'>
+            <input type='hidden' name='csrf_token' value='" . $_SESSION["csrf_token"] . "'>
             <label>Username: </label><input type='text' name='username' autocomplete='username' maxlength='32' required></input></br>
             <label>Password: </label><input type='password' name='password' autocomplete='current-password' required></input></br>
             <input type='submit' value='Log in' id='button'></input>
