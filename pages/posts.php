192764cb94c90aecb1a84b9a8d2ed940be4db1ba
rainier39 <196280087+rainier39@users.noreply.github.com>
2026-07-10 13:57:56 -0700

    Make register, posts pages use templates

--- pages/posts.php
+++ pages/posts.php
@@ -22,12 +22,13 @@
 // Only load the page if it's being requested via the index file.
 if (!defined('INDEX')) exit;
 
-$content = "";
-$title = "Posts";
+$title = lang("global.posts");
+
+$postsvars = array("posts" => "");
 
 if (!checkPerm(PERM_VIEW_POSTS)) {
-    $content .= error("You don't have permission to view posts.");
-    render($content, $title);
+    $messages[] = error("You don't have permission to view posts.");
+    render_page("", $postsvars, $title);
     exit();
 }
 
@@ -36,45 +37,39 @@ $id = $_SESSION["id"] ?? 0;
 // Get all of the blog posts.
 $posts = $db->query("SELECT `id`, `title`, `account` FROM `posts` WHERE (published='1' OR (published='0' AND account='" . $id . "')) ORDER BY `id` DESC");
 
-// Display the allPosts fieldset.
-$content .= "<fieldset class='posts'><legend>All Posts</legend>";
-
 // If there are posts, display them.
 if ($posts->num_rows > 0) {
-    $content .= "<table class='postsTable'><tbody>";
+    $postsvars["posts"] .= "<table class='postsTable'><tbody>";
     $counter = 0;
     $total = 0;
     // Display the posts.
     while ($p = $posts->fetch_assoc()) {
         if ($counter == 0) {
-            $content .= "<tr>";
+            $postsvars["posts"] .= "<tr>";
         }
         if ($counter == 5) {
-            $content .= "</tr><tr>";
+            $postsvars["posts"] .= "</tr><tr>";
             $counter = 0;
         }
-        $content .= displayPost($p["id"], $p["title"], $p["account"]);
+        $postsvars["posts"] .= displayPost($p["id"], $p["title"], $p["account"]);
         $counter++;
         $total++;
     }
     while (($total > 0) and ($total % 5)) {
-        $content .= "<td class='dummyTile'></td>";
+        $postsvars["posts"] .= "<td class='dummyTile'></td>";
         $total++;
         if (!$total % 5) {
-            $content .= "</tr>";
+            $postsvars["posts"] .= "</tr>";
         }
     }
-    $content .= "</tbody></table>";
+    $postsvars["posts"] .= "</tbody></table>";
 }
 // Otherwise print a message.
 else {
-    $content .= info("No posts yet.");
+    $postsvars["posts"] .= info("No posts yet.");
 }
 
-// End the fieldset.
-$content .= "</fieldset>";
-
-render($content, $title);
+render_page("posts.html", $postsvars, $title);
 
 ?>
 
--- pages/register.php
+++ pages/register.php
@@ -22,97 +22,99 @@
 // Only load the page if it's being requested via the index file.
 if (!defined('INDEX')) exit;
 
-$content = "";
-$title = "Register";
+$registervars = array();
+$title = lang("global.register");
 
 // If the user is already logged in, don't let them into the page.
 if (isset($_SESSION["logged_in"]) && ($_SESSION["logged_in"] === true)) {
-    $content .= error("You're already logged in.");
+    $messages[] = error("You're already logged in.");
+    render_page("", $registervars, $title);
+    exit();
 }
+
 // If registration is disabled, don't let them in.
-elseif (!$config["allowRegistration"]) {
-    $content .= error("Registration is disabled.");
+if (!$config["allowRegistration"]) {
+    $messages[] = error("Registration is disabled.");
+    render_page("", $registervars, $title);
+    exit();
+
 }
-// Otherwise, proceed as normal.
-else {
-    // Keep track of whether or not the user successfully registered.
-    $registerSuccess = false;
 
-    // Handle requests.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // If the CSRF token is sent and valid.
-        if ((isset($_POST["csrf_token"])) and ($_POST["csrf_token"] === $_SESSION["csrf_token"])) {
-            // Generate a new token.
-            generateCSRFToken();
-            
-            $errors = array();
-            
-            // Make sure there aren't too many accounts from this IP.
-            $ipCheck = $db->query("SELECT `jointime` FROM `accounts` WHERE `ip`='" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "' OR `joinip`='" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "'");
-            if ($ipCheck->num_rows >= $config["accountsPerIP"]) {
-                $errors[] = "You've made too many accounts.";
-            }
-            // Enforce a time based rate limit.
-            while ($r = $ipCheck->fetch_assoc()) {
-                if ((time()-$r["jointime"]) <= $config["accountCooldown"]) {
-                    $errors[] = "You've made an account too recently. Wait a while and try again.";
-                }
-            }
-            
-            // Validate their name.
-            $errors = array_merge($errors, validateName($_POST["name"] ?? ""));
-            
-            // Validate their username.
-            $errors = array_merge($errors, validateUsername($_POST["username"] ?? ""));
-            
-            // Validate their email.
-            $errors = array_merge($errors, validateEmail($_POST["email"] ?? "", true));
-            
-            // Make sure their password is long enough.
-            if (strlen($_POST["password"]) < 8) {
-                $errors[] = "Your password isn't long enough. Make sure your password is at least 8 characters in length.";
-            }
-            // Make sure their password entries match.
-            if ($_POST["password"] != $_POST["repeatpassword"]) {
-                $errors[] = "Your passwords don't match. Please try again.";
-            }
-            
-            // If everything checks out, make the account.
-            if (count($errors) === 0) {
-                // Insert the account into the database.
-                $now = time();
-                $db->query("INSERT INTO `accounts` (`username`, `email`, `password`, `name`, `role`, `joinip`, `ip`, `jointime`, `lastactive`) VALUES ('" . $db->real_escape_string($_POST["username"]) . "', '" . $db->real_escape_string($_POST["email"]) . "', '" . $db->real_escape_string(password_hash($_POST["password"], PASSWORD_DEFAULT)) . "', '" . $db->real_escape_string($_POST["name"]) . "', 'Unapproved', '" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "', '" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "', '" . $now . "', '" . $now . "')");
+// Keep track of whether or not the user successfully registered.
+$registerSuccess = false;
 
-                // Inform the user that they've successfully registered.
-                $content .= success("You've successfully registered for an account. Note that it must be approved before it's usable.");
-                $registerSuccess = true;
+// Handle requests.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // If the CSRF token is sent and valid.
+    if ((isset($_POST["csrf_token"])) and ($_POST["csrf_token"] === $_SESSION["csrf_token"])) {
+        // Generate a new token.
+        generateCSRFToken();
+            
+        $errors = array();
+
+        // Make sure there aren't too many accounts from this IP.
+        $ipCheck = $db->query("SELECT `jointime` FROM `accounts` WHERE `ip`='" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "' OR `joinip`='" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "'");
+        if ($ipCheck->num_rows >= $config["accountsPerIP"]) {
+            $errors[] = "You've made too many accounts.";
+        }
+        // Enforce a time based rate limit.
+        while ($r = $ipCheck->fetch_assoc()) {
+            if ((time()-$r["jointime"]) <= $config["accountCooldown"]) {
+                $errors[] = "You've made an account too recently. Wait a while and try again.";
             }
-            // Otherwise, display the errors.
-            else {
-                foreach ($errors as $e) {
-                    $content .= error($e);
-                }
+        }
+            
+        // Validate their name.
+        $errors = array_merge($errors, validateName($_POST["name"] ?? ""));
+           
+        // Validate their username.
+        $errors = array_merge($errors, validateUsername($_POST["username"] ?? ""));
+            
+        // Validate their email.
+        $errors = array_merge($errors, validateEmail($_POST["email"] ?? "", true));
+           
+        // Make sure their password is long enough.
+        if (strlen($_POST["password"]) < 8) {
+            $errors[] = "Your password isn't long enough. Make sure your password is at least 8 characters in length.";
+        }
+        // Make sure their password entries match.
+        if ($_POST["password"] != $_POST["repeatpassword"]) {
+            $errors[] = "Your passwords don't match. Please try again.";
+        }
+            
+        // If everything checks out, make the account.
+        if (count($errors) === 0) {
+            // Insert the account into the database.
+            $now = time();
+            $db->query("INSERT INTO `accounts` (`username`, `email`, `password`, `name`, `role`, `joinip`, `ip`, `jointime`, `lastactive`) VALUES ('" . $db->real_escape_string($_POST["username"]) . "', '" . $db->real_escape_string($_POST["email"]) . "', '" . $db->real_escape_string(password_hash($_POST["password"], PASSWORD_DEFAULT)) . "', '" . $db->real_escape_string($_POST["name"]) . "', 'Unapproved', '" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "', '" . $db->real_escape_string($_SERVER["REMOTE_ADDR"]) . "', '" . $now . "', '" . $now . "')");
+
+            // Inform the user that they've successfully registered.
+            $messages[] = success("You've successfully registered for an account. Note that it must be approved before it's usable.");
+            $registerSuccess = true;
+        }
+        // Otherwise, display the errors.
+        else {
+            foreach ($errors as $e) {
+                $messages[] = error($e);
             }
         }
     }
-    // Display the registration form if the user didn't successfully register.
-    if (!$registerSuccess) {
-        $content .= "<div class='form registerForm'>
-            <h1>Register</h1>
-            <form method='post'>
-                <input type='hidden' name='csrf_token' value='" . $_SESSION["csrf_token"] . "'>
-                <label for='name'>Name: </label><input type='text' name='name' id='name' autocomplete='name' maxlength='64' value='" . (isset($_POST["name"]) ? htmlspecialchars($_POST["name"]) : "") . "' required></input></br>
-                <label for='username'>Username: </label><input type='text' name='username' id='username' autocomplete='username' maxlength='32' value='" . (isset($_POST["username"]) ? htmlspecialchars($_POST["username"]) : "") . "' required></input></br>
-                <label for='email'>Email Address: </label><input type='email' name='email' id='email' autocomplete='email' maxlength='64' value='" . (isset($_POST["email"]) ? htmlspecialchars($_POST["email"]) : "") . "' required></input></br>
-                <label for='password'>Password: </label><input type='password' name='password' id='password' autocomplete='new-password' value='" . (isset($_POST["password"]) ? htmlspecialchars($_POST["password"]) : "") . "' required></input></br>
-                <label for='repeatpassword'>Repeat password: </label><input type='password' name='repeatpassword' id='repeatpassword' value='" . (isset($_POST["repeatpassword"]) ? htmlspecialchars($_POST["repeatpassword"]) : "") . "' required></input></br>
-                <br><input type='submit' value='Register' class='button'></input>
-            </form>
-        </div>";
-    }
+}
+// Display the registration form if the user didn't successfully register.
+if (!$registerSuccess) {
+    $registervars = array("token" => $_SESSION["csrf_token"],
+    "name" => $_POST["name"] ?? "",
+    "username" => $_POST["username"] ?? "",
+    "email" => $_POST["email"] ?? "",
+    "password" => $_POST["password"] ?? "",
+    "repeatpassword" => $_POST["repeatpassword"] ?? "");
+}
+else {
+    render_page("", $registervars, $title);
+    exit();
 }
 
-render($content, $title);
+render_page("register.html", $registervars, $title);
 
 ?>
 
